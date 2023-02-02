3a82afcb36001bebb9a999671528d0ea719795ed
NourSabry <70017285+NourSabry@users.noreply.github.com>
2023-02-02 16:32:52 +0200

    chat and mesages

--- lib/features/chat/repoistories/chat_repository.dart
+++ lib/features/chat/repoistories/chat_repository.dart
@@ -1,7 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:uuid/uuid.dart';
+import 'package:whats_up/common/enums/message_enum.dart';
 import 'package:whats_up/common/utils/utils.dart';
+import 'package:whats_up/models/chat_contact.dart';
+import 'package:whats_up/models/message.dart';
 import 'package:whats_up/models/user_model.dart';
 
 class ChatRepository {
@@ -17,7 +21,70 @@ class ChatRepository {
     DateTime timeSent,
     String recieverUserId,
   ) async {
-    
+    var recieverChatContact = ChatContact(
+      name: senderUserData.name,
+      profilePic: senderUserData.profilePic,
+      contactId: senderUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+    //go to firestore, go to users, with the reciever id go to the rceiver chat, and
+    //finally save our chat there
+    await firestore
+        .collection('users')
+        .doc(recieverUserId)
+        .collection('chats')
+        .doc(auth.currentUser!.uid)
+        .set(
+          recieverChatContact.toMap(),
+        );
+
+    var senderChatContact = ChatContact(
+      name: recieverUserData.name,
+      profilePic: recieverUserData.profilePic,
+      contactId: recieverUserData.uid,
+      timeSent: timeSent,
+      lastMessage: text,
+    );
+
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .collection('chats')
+        .doc(recieverUserId)
+        .set(
+          senderChatContact.toMap(),
+        );
+  }
+
+  void _saveMessageToMessageSubCollection({
+    required String receiverUderId,
+    required String text,
+    required DateTime timeSent,
+    required String messageId,
+    required String userName,
+    required String receiverUserName,
+    required MessageEnum messageType,
+  }) async {
+    final message = Message(
+      senderId: auth.currentUser!.uid,
+      recieverId: receiverUderId,
+      text: text,
+      type: messageType,
+      timeSent: timeSent,
+      messageId: messageId,
+      isSeen: false,
+    );
+    await firestore
+        .collection('users')
+        .doc(auth.currentUser!.uid)
+        .collection('chats')
+        .doc(receiverUderId)
+        .collection('messages')
+        .doc(messageId)
+        .set(
+          message.toMap(),
+        );
   }
 
 //if we want to do a unit testing we should't make it like that insead,
@@ -34,6 +101,26 @@ class ChatRepository {
       var userDataMap =
           await firestore.collection('users').doc(recieverUserId).get();
       recieverUserData = UserModel.fromMap(userDataMap.data()!);
+
+// it will generate uuid based on time
+      var messageId = const Uuid().v1();
+      saveDataToContactsSubCollection(
+        senderUser,
+        recieverUserData,
+        text,
+        timeSent,
+        recieverUserId,
+      );
+
+      _saveMessageToMessageSubCollection(
+        receiverUderId: recieverUserId,
+        text: text,
+        timeSent: timeSent,
+        messageId: messageId,
+        userName: senderUser.name,
+        receiverUserName: recieverUserData.name,
+        messageType: MessageEnum.text,
+      );
     } catch (e) {
       showSnackBar(context: context, content: e.toString());
     }
---
+++ lib/models/message.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+ 
+import 'package:whats_up/common/enums/message_enum.dart';
+
+class Message {
+  final String senderId;
+  final String recieverId;
+  final String text;
+  final MessageEnum type;
+  final DateTime timeSent;
+  final String messageId;
+  final bool isSeen;
+  Message({
+    required this.senderId,
+    required this.recieverId,
+    required this.text,
+    required this.type,
+    required this.timeSent,
+    required this.messageId,
+    required this.isSeen,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'senderId': senderId,
+      'recieverId': recieverId,
+      'text': text,
+      'type': type.type,
+      'timeSent': timeSent.millisecondsSinceEpoch,
+      'messageId': messageId,
+      'isSeen': isSeen,
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      senderId: map['senderId'] as String,
+      recieverId: map['recieverId'] as String,
+      text: map['text'] as String,
+      type:  (map['type'] as String ).toEnum(),
+      timeSent: DateTime.fromMillisecondsSinceEpoch(map['timeSent'] as int),
+      messageId: map['messageId'] as String,
+      isSeen: map['isSeen'] as bool,
+    );
+  }
+}
